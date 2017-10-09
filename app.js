32a24211a30463815ac32828cef47d20bbecf9ee
Ryan <ryan.j.porterr@gmail.com>
2017-10-09 05:29:09 -0300

    Update app.js
    Added comments for more clarity

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const Discord = require('discord.io')
 const config = require('./config')
 
 const bot = new Discord.Client({
+    // creates a bot obj with the API token from the config file
     token: config.token,
     autorun: true
 });
@@ -16,12 +17,16 @@ forBot = function(message){
 }
 
 bot.on('ready', function() {
+    // ready message to console
     console.log('Logged in as %s - %s\n', bot.username, bot.id);
 });
 
 bot.on('message', function(user, userID, channelID, message, event){
+    // responds to any message that the bot can see e.i. channels it's logged in on.
     if (forBot(message)){
+        // checks if the message starts with the sig string from the config file.
         if (message === "ping") {
+            // simple ping pong test response 
             bot.sendMessage({
                 to: channelID,
                 message: "pong"
